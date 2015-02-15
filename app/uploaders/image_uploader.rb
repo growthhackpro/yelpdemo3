a7efe6bbee3e02b0ce6d4ff4f0ac488fd84691bc
Oscar Garcia <ogarcia@growthhackerpro.com>
2015-02-15 15:40:06 -0430

    Resize Images modified

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,8 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
+  include CarrierWave::MiniMagick
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
